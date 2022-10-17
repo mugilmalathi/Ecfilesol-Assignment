6d4e278d1c544fdca39f0084770d8bc764f97f98
mugilmalathi <mugilarasan.malathi@gmail.com>
2022-10-17 15:43:45 +0530

    Basic Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Account/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
